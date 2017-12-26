8e439ef11234132b464c53480854a739853636d5
shensx <shenshuangxi@gionee.com>
2017-12-26 20:16:11 +0800

    修改

--- ubuntu-xenial-gitlab/start-host.sh
+++ ubuntu-xenial-gitlab/start-host.sh
@@ -1,5 +1,6 @@
 docker run --name='gitlab'  -d \
   -h '192.168.137.4' \
+  --restart always \
   -p 10022:10022 -p 10080:80 -p 10443:443 \
   -v /home/shenshuangxi/share/git/ubuntu-xenial-gitlab/config:/etc/gitlab \
   -v /home/shenshuangxi/share/git/ubuntu-xenial-gitlab/logs:/var/log/gitlab \
