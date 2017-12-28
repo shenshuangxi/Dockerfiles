bc38556055ae2fe32f375d5b6e3d4f1c80308982
Administrator <admin@example.com>
2017-12-28 19:54:41 +0800

    添加nginx

--- jenkins-node.sh
+++ jenkins-node.sh
@@ -11,21 +11,41 @@ if [ -d "$PWD/$1" ]; then
         mkdir docker
         touch docker/Dockerfile
 
-        cp $1.war docker/
-        mv $1.war  $1-`date +%Y%m%d%H%M%S`.war
+        cp $1.tar.gz docker/
+        mv $1.tar.gz  $1-`date +%Y%m%d%H%M%S`.tar.gz
         cd docker
 
         echo $PWD
 
-        echo "FROM shenshuangxi/ubuntu-xenial-tomcat8" >> Dockerfile
+        echo "FROM shenshuangxi/ubuntu-xenial-nginx" >> Dockerfile
         echo "MAINTAINER shenshuangxi <1695904589@qq.com>" >> Dockerfile
         echo "ENV REFRESHED_AT 2017-12-22" >> Dockerfile
-        echo "VOLUME [\"/data/$1\"]" >> Dockerfile
-        echo "RUN rm -f -d -r /home/apache-tomcat-8.0.48/webapps/*" >> Dockerfile
-        echo "COPY $1.war /home/apache-tomcat-8.0.48/webapps/" >> Dockerfile
-        echo "RUN mv /home/apache-tomcat-8.0.48/webapps/$1.war /home/apache-tomcat-8.0.48/webapps/ROOT.war" >> Dockerfile
-
+		echo "" >> Dockerfile
+        echo "WORKDIR /home" >> Dockerfile
+        echo "COPY $1.tar.gz /usr/local/nginx/" >> Dockerfile
+		echo "WORKDIR /usr/local/nginx/" >> Dockerfile
+		echo "RUN tar -zxvf $1.tar.gz" >> Dockerfile
+		echo "RUN rm $1.tar.gz" >> Dockerfile
+		echo "" >> Dockerfile
+		echo "WORKDIR /usr/local/nginx/conf" >> Dockerfile
+		echo "RUN echo \"worker_processes  1;\" > nginx.conf " >> Dockerfile
+		echo "RUN echo \"events { worker_connections  1024; }\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"http {\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\tinclude       mime.types;\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\tdefault_type  application/octet-stream;\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\tsendfile        on;\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\tserver {\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\t\tlisten       80;\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\t\tserver_name  localhost;\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\t\tlocation / { root   $1; index  index.html index.htm; }\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\t\terror_page   500 502 503 504  /index.html;\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\t\tlocation = /index.html { root   $1; }\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\t\tlocation /api/{proxy_pass http://192.168.137.4:10097;}\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"\t}\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"}\" >> nginx.conf " >> Dockerfile
+		echo "RUN echo \"daemon off;\" >> nginx.conf " >> Dockerfile
+		echo "WORKDIR /home" >> Dockerfile
 
         docker build -t shenshuangxi/$1 .
-        docker run -d -p $2:8080 --name shenshuangxi$1  shenshuangxi/$1
+        docker run -d -p $2:80 --name shenshuangxi$1  shenshuangxi/$1
 fi
